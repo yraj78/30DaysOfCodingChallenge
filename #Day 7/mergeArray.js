8520a30990c14bb24a499b2ef691b68084c665cd
Yash Chaturvedi <43944057+yraj78@users.noreply.github.com>
2023-09-08 22:56:32 +0530

    Create mergeArray.js

---
+++ #Day 7/mergeArray.js
@@ -0,0 +1,8 @@
+// initializing two arrays 
+ const arr1 = [1, 2, 3]; 
+ const arr2 = [4, 5, 6]; 
+  
+ // Merging Both Arrays 
+ const mergedArray = arr1.concat(arr2); 
+  
+ console.log(mergedArray); // Output : [ 1, 2, 3, 4, 5, 6 ]
