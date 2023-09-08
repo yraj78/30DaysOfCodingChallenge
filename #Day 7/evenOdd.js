229c3ec800d237fdb0a8559352f2ff664e2c33bb
Yash Chaturvedi <43944057+yraj78@users.noreply.github.com>
2023-09-08 22:57:03 +0530

    Create evenOdd.js

---
+++ #Day 7/evenOdd.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5]; 
+  
+ arr.forEach((e)=>{ 
+     if(e%2==0){ 
+         console.log(e,"is Even"); 
+     }else{ 
+         console.log(e,"is Odd"); 
+     } 
+ }) 
+  
+ // Output : 
+ // 1 is Odd 
+ // 2 is Even 
+ // 3 is Odd 
+ // 4 is Even 
+ // 5 is Odd
